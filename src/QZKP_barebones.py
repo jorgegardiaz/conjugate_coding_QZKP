8f9a1f8686a7e1533e16f7a2d1fe5360ad3ca209
jorgegardiaz <160606247+jorgegardiaz@users.noreply.github.com>
2025-03-15 12:20:12 +0000

    Add files via upload

---
+++ src/QZKP_barebones.py
@@ -0,0 +1,124 @@
+from qiskit import QuantumCircuit # qiskit 1.3.1
+from qiskit_aer import AerSimulator # qiskit_aer 0.16.0
+import sys
+sim = AerSimulator()
+
+#----------------------------------------
+# Funcitons
+#----------------------------------------
+def quantum_random_binary_string(length):
+    '''
+    Random key generator using quantum randomness.
+    '''
+    string = []
+    for bit in range(length):
+        qcoin = QuantumCircuit(1,1)
+        qcoin.h(0)
+        qcoin.measure(0,0)
+        exec = sim.run(qcoin, shots=1).result()
+        string.append(int(list(exec.get_counts(qcoin).keys())[0]))
+    return string
+
+
+def psi_gen(w, basis):
+    '''
+    Generation of the quantum state |psi>.
+    '''
+    if len(w) != len(basis):
+        raise ValueError('Same number of basis and bits expected.')
+    psi = []
+    for i in range(len(w)):
+        qubit = QuantumCircuit(1, 1)
+        if w[i] == 1:
+            qubit.x(0)
+        if basis[i] == 1:
+            qubit.h(0)
+        psi.append(qubit)
+    return psi
+
+def challenge_gen(psi, c):
+    '''
+    Generation of the challenge for |psi>.
+    '''
+    if len(psi) != len(c):
+        raise ValueError('Same number of qubits and bits expected.')
+    for i in range(len(psi)):
+        if c[i] == 1:
+            psi[i].h(0)
+    return psi
+
+def zk_mod(psi, p):
+    if len(psi) != len(p):
+        raise ValueError('Same number of qubits and bits expected.')
+    for i in range(len(psi)):
+        if p[i] == 1:
+            psi[i].h(0)
+    return psi
+
+def measurements(psi, basis):
+    if len(psi) != len(basis):
+        raise ValueError('Same number of qubits and basis expected.')
+    results = []
+    for i in range(len(psi)):
+        if basis[i] == 1:
+            psi[i].h(0)
+        psi[i].measure(0, 0)
+        exec = sim.run(psi[i], shots=1).result()
+        result = int(list(exec.get_counts(psi[i]).keys())[0])
+        results.append(result)
+    return results
+
+def c_aprox_gen(results, p, w):
+    c_aprox = []
+    for i, bit in enumerate(results):
+        # if bit == w[i] gamma[i]; else !gamma[i]
+        decission = int(p[i] ^ (bit != w[i]))
+        c_aprox.append(decission)
+    return c_aprox
+
+def equal_entries_percentage(list1, list2):
+    if len(list1) != len(list2):
+        raise ValueError("The lists must have the same length.")
+    equals = 0
+    for (a, b) in zip(list1, list2):
+        equals += int(a == b)
+    return (equals / len(list1)) * 100
+
+#----------------------------------------
+# Protocol execution
+#----------------------------------------
+
+# 1. Keys generation (this keys could be shared through QKD)
+
+
+
+if __name__ == '__main__':
+
+    key_length = int(sys.argv[1])
+    basis = quantum_random_binary_string(key_length)
+    w = quantum_random_binary_string(key_length)
+
+    # 2. Preparation of the challenge (Bob)
+    psi = psi_gen(w, basis) # |psi> state generation from w and basis
+
+    c = quantum_random_binary_string(key_length) # Random generation for c
+    psi = challenge_gen(psi, c) # Challenge setup
+
+    # After this, Bob sends the modified qubits to Alice 
+
+    # 3. Zero-Knowledge modifications (Alice)
+    p = quantum_random_binary_string(key_length) # Random generation for p
+    psi = zk_mod(psi, p)
+
+    # 4. Measurments (Alice)
+    results = measurements(psi, basis) # Alice measures |psi> using the basis
+
+    # 5. Building the approximation for c (Alice)
+    c_aprox = c_aprox_gen(results, p, w)
+
+    # After this, Alice sends her approximation of c to Bob
+
+    # 6. Coincidence precentage (Bob)
+    equal_percentage = equal_entries_percentage(c, c_aprox)
+
+    print(f'{equal_percentage}% de acierto para clave de longitud {key_length} bits.')
---
+++ src/QZKP_ideal.py
@@ -0,0 +1,211 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import time
+import sys
+sim = AerSimulator()
+
+#----------------------------------------
+# Auxiliary functions
+#----------------------------------------
+def quantum_random_binary_string(length):
+    '''
+    Random key generator using quantum randomness.
+    '''
+    string = []
+    for bit in range(length):
+        qcoin = QuantumCircuit(1,1)
+        qcoin.h(0)
+        qcoin.measure(0,0)
+        exec = sim.run(qcoin, shots=1).result()
+        string.append(int(list(exec.get_counts(qcoin).keys())[0]))
+    return string
+
+
+def psi_gen(w, basis):
+    '''
+    Generation of the quantum state |psi>.
+    '''
+    if len(w) != len(basis):
+        raise ValueError('Same number of basis and bits expected.')
+    psi = []
+    for i in range(len(w)):
+        qubit = QuantumCircuit(1, 1)
+        if w[i] == 1:
+            qubit.x(0)
+        if basis[i] == 1:
+            qubit.h(0)
+        psi.append(qubit)
+    return psi
+
+def challenge_gen(psi, c):
+    '''
+    Generation of the challenge for |psi>.
+    '''
+    if len(psi) != len(c):
+        raise ValueError('Same number of qubits and bits expected.')
+    for i in range(len(psi)):
+        if c[i] == 1:
+            psi[i].h(0)
+    return psi
+
+def zk_mod(psi, p):
+    '''
+    Alice Zero-Knowledge momdifications to the state |psi>.
+    '''
+    if len(psi) != len(p):
+        raise ValueError('Same number of qubits and bits expected.')
+    for i in range(len(psi)):
+        if p[i] == 1:
+            psi[i].h(0)
+    return psi
+
+def measurements(psi, basis):
+    '''
+    Alice measures using the secret basis.
+    '''
+    if len(psi) != len(basis):
+        raise ValueError('Same number of qubits and basis expected.')
+    results = []
+    for i in range(len(psi)):
+        if basis[i] == 1:
+            psi[i].h(0)
+        psi[i].measure(0, 0)
+        exec = sim.run(psi[i], shots=1).result()
+        result = int(list(exec.get_counts(psi[i]).keys())[0])
+        results.append(result)
+    return results
+
+def c_aprox_gen(results, p, w):
+    '''
+    Generation of the approximation c' for c.
+    '''
+    c_aprox = []
+    for i, bit in enumerate(results):
+        # if bit == w[i] gamma[i]; else !gamma[i]
+        decission = int(p[i] ^ (bit != w[i]))
+        c_aprox.append(decission)
+    return c_aprox
+
+def equal_entries_percentage(list1, list2):
+    '''
+    Percentage of equal entries.
+    '''
+    if len(list1) != len(list2):
+        raise ValueError("The lists must have the same length.")
+    equals = 0
+    for (a, b) in zip(list1, list2):
+        equals += int(a == b)
+    return (equals / len(list1)) * 100
+
+def random_binary_string(length):
+    '''
+    Pseudo random binary strings.
+    '''
+    return [random.choice([0, 1]) for _ in range(length)]
+
+def loading_bar(iteration, total, start_time, prefix='Progress:', length=50, fill='█', print_end='\r'):
+    """
+    Actualiza una barra de progreso en cada iteración de un bucle, con un contador de tiempo.
+
+    Args:
+        iteration (int): Iteración actual (debe comenzar en 0).
+        total (int): Número total de iteraciones.
+        start_time (float): Tiempo inicial (resultado de time.time()).
+        prefix (str): Texto que se muestra antes de la barra.
+        length (int): Longitud de la barra.
+        fill (str): Carácter utilizado para llenar la barra.
+        print_end (str): Carácter para terminar el print, usa '\r' para sobreescribir.
+    """
+    percent = 100 * (iteration / total)
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    elapsed_time = time.time() - start_time
+    print(f'\r{prefix} |{bar}| {percent:.1f}% Elapsed: {elapsed_time:.1f}s', end=print_end)
+    if iteration == total:
+        print()
+
+#----------------------------------------
+# Protocol execution
+#----------------------------------------
+if __name__=='__main__':
+    
+    key_length = int(sys.argv[1])
+    num_iter = int(sys.argv[2])
+    percentages = []
+
+    start_time = time.time()
+
+    basis = quantum_random_binary_string(key_length)
+    w = quantum_random_binary_string(key_length)
+
+
+    iter=1
+    for i in range(num_iter):
+        dec = random.choice([0, 1])
+        # 1. Keys generation (this keys could be shared through QKD)
+
+        # 2. Preparation of the challenge (Bob)
+        psi = psi_gen(w, basis) # |psi> state generation from w and basis
+
+        c = quantum_random_binary_string(key_length) # Random generation for c
+        psi = challenge_gen(psi, c) # Challenge setup
+
+        # After this, Bob sends the modified qubits to Alice 
+
+
+        if dec == 0:
+            # Honest prover Alice
+
+            # 3. Zero-Knowledge modifications (Alice)
+            p = quantum_random_binary_string(key_length) # Random generation for p
+            psi = zk_mod(psi, p)
+
+            # 4. Measurments (Alice)
+            results = measurements(psi, basis) # Alice measures |psi> using the basis
+
+            # 5. Building the approximation for c (Alice)
+            c_aprox = c_aprox_gen(results, p, w)
+
+            # After this, Alice sends her approximation of c to Bob
+
+            # 6. Coincidence proportion (Bob)
+            equal_percentage = equal_entries_percentage(c, c_aprox)
+            percentages.append((equal_percentage, dec))
+
+        else:
+            # Dishonest prover Eve
+            c_aprox = random_binary_string(key_length)
+            equal_percentage = equal_entries_percentage(c, c_aprox)
+            percentages.append((equal_percentage, dec))
+        loading_bar(iter, num_iter, start_time)
+        iter +=1
+
+    #----------------------------------------
+    # Graphs
+    #----------------------------------------
+    equal_percentages = [x[0] for x in percentages]
+    decisions = [x[1] for x in percentages]
+    iters = range(1,num_iter + 1)
+    results = pd.DataFrame({'Iteration': iters, 'Decision': decisions, 'Percentages': equal_percentages})
+    results.to_csv(f'iter_data_{key_length}_{num_iter}.csv', index=False)
+
+    # Separate honest iteration and dishonest iterations
+    honest_data = [percentage for percentage, dec in percentages if dec == 0]
+    dishonest_data = [percentage for percentage, dec in percentages if dec == 1]
+    
+    # Count values
+    count_honest = pd.Series(honest_data).value_counts()
+    count_dishonest = pd.Series(dishonest_data).value_counts()
+
+    # Convert to DataFrame
+    df_honest = pd.DataFrame({'Percentage': count_honest.index, 'Frecuency': count_honest.values})
+    df_dishonest = pd.DataFrame({'Percentage': count_dishonest.index, 'Frecuency': count_dishonest.values})
+
+    # Guardar en un archivo CSV
+    df_honest.to_csv(f'honest_data_{key_length}_{num_iter}.csv', index=False)
+    df_dishonest.to_csv(f'dishonest_data_{key_length}_{num_iter}.csv', index=False)
